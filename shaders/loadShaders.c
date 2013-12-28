5ad812a63189ec3395763c526e4b3ced4208b5e8
Ed Markowski <gordotron9000@gmail.com>
2013-12-28 14:08:44 -0800

    tabs to spaces

--- shaders/loadShaders.c
+++ shaders/loadShaders.c
@@ -4,141 +4,141 @@
 // Creates and returns a shader that is ready to be linked and compiled.
 GLuint createShader(GLenum shaderType, const char *fileName)
 {
-	GLuint shader = glCreateShader(shaderType);
+    GLuint shader = glCreateShader(shaderType);
 
-	// allocate memory for the file data	
-	char *fileData; 
-	int characters;
-	if (characters = countChars(fileName))
-		fileData = (char *)malloc(sizeof(char)*countChars(fileName));
-	else
-	{
-		// no data
-		fprintf(stderr, "Shader %s contains no data...\n", fileName);
-		fprintf(stderr, "Closing program...\n");
-		exit(EXIT_FAILURE);
-	}
+    // allocate memory for the file data    
+    char *fileData; 
+    int characters;
+    if (characters = countChars(fileName))
+        fileData = (char *)malloc(sizeof(char)*countChars(fileName));
+    else
+    {
+        // no data
+        fprintf(stderr, "Shader %s contains no data...\n", fileName);
+        fprintf(stderr, "Closing program...\n");
+        exit(EXIT_FAILURE);
+    }
 
-	// store data from fileName into fileData
-	storeData(fileData, fileName);
-	// fileData is now the source code for the shader	
-	glShaderSource(shader, 1, (const GLchar**)&fileData, NULL);
-	if (DEBUG) fprintf(stdout, "Compiling shader %s\n", fileName);
-	glCompileShader(shader);
+    // store data from fileName into fileData
+    storeData(fileData, fileName);
+    // fileData is now the source code for the shader    
+    glShaderSource(shader, 1, (const GLchar**)&fileData, NULL);
+    if (DEBUG) fprintf(stdout, "Compiling shader %s\n", fileName);
+    glCompileShader(shader);
 
-	// check compile status
-	GLint status;
-	glGetShaderiv(shader, GL_COMPILE_STATUS, &status);
-	if (GL_FALSE == status)
-	{
-		GLint infoLogLength;
-		glGetShaderiv(shader, GL_INFO_LOG_LENGTH, &infoLogLength);
-		GLchar *strInfoLog = 
-			(GLchar *)malloc(sizeof(GLchar)*(infoLogLength+1));
-		glGetShaderInfoLog(shader, infoLogLength, NULL, strInfoLog);
+    // check compile status
+    GLint status;
+    glGetShaderiv(shader, GL_COMPILE_STATUS, &status);
+    if (GL_FALSE == status)
+    {
+        GLint infoLogLength;
+        glGetShaderiv(shader, GL_INFO_LOG_LENGTH, &infoLogLength);
+        GLchar *strInfoLog = 
+            (GLchar *)malloc(sizeof(GLchar)*(infoLogLength+1));
+        glGetShaderInfoLog(shader, infoLogLength, NULL, strInfoLog);
 
-		const char *strShaderType = NULL;
-		switch (shaderType)
-		{
-			case GL_VERTEX_SHADER: strShaderType = "vertex"; break;
-			case GL_FRAGMENT_SHADER: strShaderType = "fragment"; break;
-			//case GL_GEOMETRY_SHADER: strShaderType = "geometry"; break;
-		}
-		fprintf(stderr, "Compile failure in %s shader:\n%s\n",
-			strShaderType, strInfoLog);
-		// free up dynamic memory
-		free(strInfoLog);
-	}
+        const char *strShaderType = NULL;
+        switch (shaderType)
+        {
+            case GL_VERTEX_SHADER: strShaderType = "vertex"; break;
+            case GL_FRAGMENT_SHADER: strShaderType = "fragment"; break;
+            //case GL_GEOMETRY_SHADER: strShaderType = "geometry"; break;
+        }
+        fprintf(stderr, "Compile failure in %s shader:\n%s\n",
+            strShaderType, strInfoLog);
+        // free up dynamic memory
+        free(strInfoLog);
+    }
 
-	// free up dynamic memory
-	free(fileData);
-	// return compiled shader value
-	return shader;
+    // free up dynamic memory
+    free(fileData);
+    // return compiled shader value
+    return shader;
 }
 
 // Links and compiles all shader source code to create main shader program.
 GLuint createProgram(GLuint *shaderList, int size)
 {
-	// establish program id
-	GLuint program = glCreateProgram();
-	// link all shaders
-	int i;
-	for (i = 0; i < size; ++i)
-		glAttachShader(program, shaderList[i]);
+    // establish program id
+    GLuint program = glCreateProgram();
+    // link all shaders
+    int i;
+    for (i = 0; i < size; ++i)
+        glAttachShader(program, shaderList[i]);
 
-	if (DEBUG) fprintf(stdout, "Linking compiled shaders\n");
-	glLinkProgram(program);
+    if (DEBUG) fprintf(stdout, "Linking compiled shaders\n");
+    glLinkProgram(program);
 
-	// determine linking status
-	GLint status;
-	glGetProgramiv(program, GL_LINK_STATUS, &status);
-	if (GL_FALSE == status) // failed to link shaders
-	{
-		GLint infoLogLength;
-		glGetProgramiv(program, GL_INFO_LOG_LENGTH, &infoLogLength);
+    // determine linking status
+    GLint status;
+    glGetProgramiv(program, GL_LINK_STATUS, &status);
+    if (GL_FALSE == status) // failed to link shaders
+    {
+        GLint infoLogLength;
+        glGetProgramiv(program, GL_INFO_LOG_LENGTH, &infoLogLength);
 
-		GLchar *strInfoLog = 
-			(GLchar *)malloc(sizeof(GLchar)*(infoLogLength+1));	
-		glGetProgramInfoLog(program, infoLogLength, NULL, strInfoLog);
-		// print error message
-		fprintf(stderr, "%s: Linker failure\n", strInfoLog);
-		// free up dynamic memory
-		free(strInfoLog);
-	}
+        GLchar *strInfoLog = 
+            (GLchar *)malloc(sizeof(GLchar)*(infoLogLength+1));    
+        glGetProgramInfoLog(program, infoLogLength, NULL, strInfoLog);
+        // print error message
+        fprintf(stderr, "%s: Linker failure\n", strInfoLog);
+        // free up dynamic memory
+        free(strInfoLog);
+    }
 
-	for (i = 0; i < size; ++i)
-		glDetachShader(program, shaderList[i]);
+    for (i = 0; i < size; ++i)
+        glDetachShader(program, shaderList[i]);
 
-	// return the program id 
-	return program;
+    // return the program id 
+    return program;
 }
 
 // Returns the number of characters in fileName.
 int countChars(const char *fileName)
 {
-	FILE *fp;
+    FILE *fp;
 
-	// open file and check if it was successful
-	if (NULL == (fp = fopen(fileName, "r")))
-	{
-		if (DEBUG) fprintf(stderr, "%s: No such file\n", fileName);
-		exit(EXIT_FAILURE);
-	}
+    // open file and check if it was successful
+    if (NULL == (fp = fopen(fileName, "r")))
+    {
+        if (DEBUG) fprintf(stderr, "%s: No such file\n", fileName);
+        exit(EXIT_FAILURE);
+    }
 
-	// count the number of characters in the 
-	char ch;
-	unsigned count = 0;
-	// count all characters in the file
-	while (EOF != (ch = fgetc(fp)))
-		++count;
+    // count the number of characters in the 
+    char ch;
+    unsigned count = 0;
+    // count all characters in the file
+    while (EOF != (ch = fgetc(fp)))
+        ++count;
 
-	// close file
-	fclose(fp);
+    // close file
+    fclose(fp);
 
-	// return the number of characters in the file
-	return count;
+    // return the number of characters in the file
+    return count;
 }
 
 // Stores contents of fileName into fileData.
 void storeData(char *fileData, const char *fileName)
 {
-	FILE *fp;
-	
-	// open file and check if it was successful
-	if (NULL == (fp = fopen(fileName, "r")))
-	{
-		if (DEBUG) fprintf(stderr, "%s: No such file\n", fileName);
-		exit(EXIT_FAILURE);
-	}
+    FILE *fp;
+    
+    // open file and check if it was successful
+    if (NULL == (fp = fopen(fileName, "r")))
+    {
+        if (DEBUG) fprintf(stderr, "%s: No such file\n", fileName);
+        exit(EXIT_FAILURE);
+    }
 
-	int index = 0;
-	char ch;
-	// copy contents of file to fileData char array
-	while (EOF != (ch = fgetc(fp)))
-		fileData[index++] = ch;
-	// set last character to the null character
-	fileData[index-1] = '\0';
-	 
-	// close file
-	fclose(fp);
+    int index = 0;
+    char ch;
+    // copy contents of file to fileData char array
+    while (EOF != (ch = fgetc(fp)))
+        fileData[index++] = ch;
+    // set last character to the null character
+    fileData[index-1] = '\0';
+     
+    // close file
+    fclose(fp);
 }
